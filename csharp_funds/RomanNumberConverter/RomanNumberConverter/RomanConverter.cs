ed5266954ad9a5ffd0511633b39fcee4aacfe8f8
Manu <manuela_karpati@epam.com>
2019-04-12 09:38:03 +0200

    Add RomanNumberConverter skeleton

---
+++ csharp_funds/RomanNumberConverter/RomanNumberConverter/RomanConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RomanNumberConverter
+{
+    public static class RomanConverter
+    {
+        public static string ToRoman(this int number)
+        {
+
+            return "";
+        }
+
+    }
+}
