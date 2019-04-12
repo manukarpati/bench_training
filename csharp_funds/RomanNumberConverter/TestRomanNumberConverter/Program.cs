d2d74da223984fcdd3e0a3a945a93069cf00cd8b
Manu <manuela_karpati@epam.com>
2019-04-12 09:38:21 +0200

    Add roman number converter tester app

---
+++ csharp_funds/RomanNumberConverter/TestRomanNumberConverter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RomanNumberConverter;
+
+namespace TestRomanNumberConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(42.ToRoman());
+            Console.WriteLine(1967.ToRoman());
+
+
+            Console.ReadKey();
+        }
+    }
+}
