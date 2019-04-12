b349971abb783018485d04f170e41d1f66be54ba
Manu <manuela_karpati@epam.com>
2019-04-12 13:09:21 +0200

    Add converter logic to ToRoman method

--- csharp_funds/RomanNumberConverter/RomanNumberConverter/RomanConverter.cs
+++ csharp_funds/RomanNumberConverter/RomanNumberConverter/RomanConverter.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RomanNumberConverter
 {
     public static class RomanConverter
     {
+        private static SortedList<int, string> romanValues = new SortedList<int, string>() {
+
+          { 1,"I"},
+          { 4, "IV"},
+          { 5, "V"},
+          { 9, "IX"},
+          { 10, "X"},
+          { 40, "XL"},
+          { 50, "L"},
+          { 90,"XC"},
+          { 100,"C"},
+          { 400, "CD"},
+          { 500, "D"},
+          { 900, "CM"},
+          { 1000, "M"},
+        };
+
         public static string ToRoman(this int number)
         {
+            if (number < 1 || number > 3999)
+            {
+                return "invalid number";
+            }
 
-            return "";
+            string result = "";
+
+            for(int i= romanValues.Count-1; i>=0; i--)
+            {
+                if(number >= romanValues.Keys[i])
+                {
+                    int times = number / romanValues.Keys[i];
+
+                    for( int j = 0; j < times; j++)
+                    {
+                        result += romanValues.Values[i];
+                    }
+
+                    number = number % romanValues.Keys[i];
+                }
+            }
+
+            return result;
         }
 
     }
+
 }
