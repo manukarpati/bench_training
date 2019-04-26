179111e4df79e5adfac87edd4fbda40b5965e471
Manu <manuela_karpati@epam.com>
2019-04-26 12:02:35 +0200

    Add SecondMax base to repo

---
+++ NJO4_generics/SecondMax/SecondMax/EnumarableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecondMax
+{
+    public static class EnumarableExtensions
+    {
+        //TODO: Put your SecondMax extension method here
+    }
+}
