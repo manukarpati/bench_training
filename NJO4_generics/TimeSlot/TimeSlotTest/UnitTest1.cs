099a8ac1b428bdd0fafdf855e64c9742e8a3264f
Manu <manuela_karpati@epam.com>
2019-04-26 15:52:15 +0200

    Remove unmatching unit test project

--- NJO4_generics/TimeSlot/TimeSlotTest/UnitTest1.cs
+++ NJO4_generics/TimeSlot/TimeSlotTest/UnitTest1.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeSlot;
 
 namespace TimeSlotTest
 {
     [TestClass]
     public class UnitTest1
     {
+
         [TestMethod]
         public void TimeSlot_TestShownSlotNumber()
         {
