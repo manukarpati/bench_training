652363e4180f5447d63f815b507ef05d588581c3
Manu <manuela_karpati@epam.com>
2019-04-29 17:38:19 +0200

    Add starter code for string array task

---
+++ NJ05_linq/StringArray/StringArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArray
+{
+    class Program
+    {
+        static string[] strings =
+                              {
+                                "You only live forever in the lights you make",
+                                "When we were young we used to say",
+                                "That you only hear the music when your heart begins to break",
+                                "Now we are the kids from yesterday"
+                              };
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
