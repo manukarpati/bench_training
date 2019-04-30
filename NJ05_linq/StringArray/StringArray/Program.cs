459b11e178ae0974c738f3732c00b6459d04a283
Manu <manuela_karpati@epam.com>
2019-04-30 09:09:10 +0200

    Add 3 more answers to task1

--- NJ05_linq/StringArray/StringArray/Program.cs
+++ NJ05_linq/StringArray/StringArray/Program.cs
@@ -18,14 +18,30 @@ namespace StringArray
 
         static void Main(string[] args)
         {
+            //Calculate of the word count of each sentences
             var wordcount = strings.Select(s => s.Count(c => c==' ')+1);
 
+            //Split the sentences into an array of words and select the ones that start with a vowel (y is not a vowel in this case)
             var vowelList = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
             var startsWithaVowel = strings.Select(s => s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
                                            .Select(s => s.Where(e => vowelList.Any(v => v == e[0])))  ;
 
-         
+            //Find the longest word
+            var longestWord = strings.SelectMany(s => s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+                                            .FirstOrDefault(s => s.Length == 
+                                                    strings.Max(ss => ss.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                                                        .Select(sss => sss.Length).ToArray().Max()));
 
+
+            //Display the average word count of the sentences
+            var averageWordCount = strings.Select(s => s.Count(c => c == ' ') + 1).ToArray().Average();
+            Console.WriteLine(averageWordCount);
+
+            //Put the words into alphabetical order and remove the duplicates (case insensitive)
+            var wordsInAlphabeticalOrder = strings.SelectMany(s => s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+                                                   .Select(s => s.ToLower())
+                                                   .ToHashSet()
+                                                   .OrderBy(s => s);
 
             Console.ReadKey();
 
