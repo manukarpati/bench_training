3f952f417a998b43878a80fd92c6652298bb4d44
Manu <manuela_karpati@epam.com>
2019-05-02 13:52:39 +0200

    Finish employee task

--- NJ05_linq/Employees/Employees/Program.cs
+++ NJ05_linq/Employees/Employees/Program.cs
@@ -31,15 +31,14 @@ namespace Employees
 
             //Display the name of the employee who earn the most
             var richestEmployee = employees.OrderByDescending(e => e.Salary)
-                                            .Take(1)
                                             .Select(e => e.Name)
                                             .FirstOrDefault();
             Console.WriteLine(richestEmployee);
             Console.WriteLine("***");
 
             //Display the name of the employees who earn less than the company average.
-            var poorEmployees = employees.Where(e => e.Salary < employees.Average(es => es.Salary)).ToList();
-            poorEmployees.ForEach(e => Console.WriteLine(e.Name));
+            var poorEmployees = employees.Where(e => e.Salary < employees.Average(es => es.Salary));
+            poorEmployees.ToList().ForEach(e => Console.WriteLine(e.Name));
             Console.WriteLine("***");
 
 
@@ -49,14 +48,18 @@ namespace Employees
             //Display the name of employees who earn the same amount and sort the result by salaries then names in an ascending order
             var sameSalaryEmployees = employees.GroupBy(e=> e.Salary)
                                                .Where(g => g.Count() > 1)
-                                               .OrderBy(g => g.FirstOrDefault().Salary)
+                                               .OrderBy(g => g.Key)
                                                .Select(g => g.OrderBy(e => e.Name))
-                                               .Select(g => g.Select(e => e.Name))
-                                               ;
+                                               .Select(g => g.Select(e => e.Name));
 
             sameSalaryEmployees.ToList().ForEach(l => l.ToList().ForEach(n => Console.WriteLine(n)));
             Console.WriteLine("*****");
 
+            //Group the employees in the following salary ranges: 200-399, 400-599, 600-799, 800-999
+            var ranges = new[] { 999, 799, 599, 399, 199 };
+            var groupedSalaryEmployees = employees.GroupBy(e => ranges.FirstOrDefault(r => r <= e.Salary))
+                                                    .OrderBy(g => g.Key);
+            groupedSalaryEmployees.ToList().ForEach(l => l.ToList().ForEach(n => Console.WriteLine(n)));
 
 
             Console.ReadKey();
