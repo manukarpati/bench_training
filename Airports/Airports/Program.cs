1a5adaa6690ac5147bcdc7b68a6cb1244ef0c59d
Manu <manuela_karpati@epam.com>
2019-05-13 15:09:47 +0200

    Add airports basics

---
+++ Airports/Airports/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Airports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
